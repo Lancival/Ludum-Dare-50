c24a290f854599d08b8b1a28874b8484c127202b
Richard Cheng <richard.hokan.cheng@gmail.com>
2022-04-02 15:16:03 -0700

    CustomOptionView registration/unregistration improvement

--- Assets/Scripts/Dialogue/CustomOptionView.cs
+++ Assets/Scripts/Dialogue/CustomOptionView.cs
@@ -8,12 +8,20 @@ public class CustomOptionView : MonoBehaviour
     [SerializeField] private TextMeshProUGUI textMesh;
     public UnityEvent onOptionChosen;
 
-    public void Start()
+    public void Start() => Register();
+    public void OnDestroy() => UnRegister();
+
+    public void Register()
     {
         if (CustomDialogueView.instance != null && !CustomDialogueView.instance.optionViews.Contains(this))
             CustomDialogueView.instance.optionViews.Add(this);
     }
-    public void OnDestroy() => CustomDialogueView.instance.optionViews.Remove(this);
+
+    public void UnRegister()
+    {
+        if (CustomDialogueView.instance != null && CustomDialogueView.instance.optionViews.Contains(this))
+            CustomDialogueView.instance.optionViews.Remove(this);
+    }
 
     public void UpdateText(string text) => textMesh.text = text;
     public void InvokeOption() => onOptionChosen.Invoke();
