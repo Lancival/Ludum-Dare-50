dc610ef1745cf322e96b3d288d64efaf4a29997f
Peter Sutarjo <peter.sutarjo@gmail.com>
2022-04-04 14:25:48 -0700

    Sections completed

---
+++ Assets/Scripts/Platforms/Elevator.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    public Transform startPosition;
+    public Transform endPosition;
+    public float moveSpeed = 10f;
+
+    private bool startElevator = false;
+
+    void FixedUpdate()
+    {
+        if (startElevator)
+            MoveToEndPoint();
+        else
+        {
+            MoveToStartPoint();
+        }
+    }
+
+    private void MoveToEndPoint()
+    {
+        this.transform.position = Vector2.MoveTowards(this.transform.position, endPosition.position, Time.deltaTime * moveSpeed);
+    }
+
+     private void MoveToStartPoint()
+    {
+        this.transform.position = Vector2.MoveTowards(this.transform.position, startPosition.position, Time.deltaTime * moveSpeed);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        startElevator = true;    
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        startElevator = false;
+    }
+}
